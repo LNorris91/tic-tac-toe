356dce89408e62e915e6f99c5879d40b9e48262e
Luke Norris <154083568+LNorris91@users.noreply.github.com>
2024-04-05 17:11:30 -0400

    Creates Check Win Function
    this commit adds a function that will check if
    a player has one the game after making a move.

--- main.js
+++ main.js
@@ -27,7 +27,7 @@ function Gameboard() {
         if (board[row][column].getValue() === 0) {
             board[row][column].addToken(player);
         } else {
-            console.log("Invalid.")
+            return
         }
     }
 
@@ -74,16 +74,37 @@ function Gameplay(player1, player2) {
     const players = Players(player1, player2)
     
     const playTurn = (row, column) => {
-        console.log(`${players.getActivePlayer().name}'s move was`);
         board.placeToken(players.getActivePlayer().token, row, column);
+        checkWin()
+        console.log(`${players.getActivePlayer().name}'s move was`);
          if (board.getBoard()[row][column].getValue() === players.getActivePlayer().token) {
               players.switchActivePlayer();
           } else {
-              console.log("Go again");
+              console.log("Invalid, go again");
             }
         board.printBoard()
-        console.log(`It is now ${players.getActivePlayer().name}'s turn`);
+        console.log(`It is ${players.getActivePlayer().name}'s turn`);
     }
+
+    const checkWin = () => {
+        const arr = board.getBoard().map((row) => row.map((cell) => cell.getValue()));
+        let activeToken = players.getActivePlayer().token;
+        console.log(activeToken)
+        
+        if (arr[0][0] === activeToken && arr[0][1] === activeToken && arr[0][2] === activeToken || //check for horizontal wins
+            arr[1][0] === activeToken && arr[1][1] === activeToken && arr[1][2] === activeToken || 
+            arr[2][0] === activeToken && arr[2][1] === activeToken && arr[2][2] === activeToken || 
+
+            arr[0][0] === activeToken && arr[1][0] === activeToken && arr[2][0] === activeToken || //check for vertical wins
+            arr[0][1] === activeToken && arr[1][1] === activeToken && arr[2][1] === activeToken || 
+            arr[0][2] === activeToken && arr[1][2] === activeToken && arr[2][2] === activeToken || 
+
+            arr[0][0] === activeToken && arr[1][1] === activeToken && arr[2][2] === activeToken || //check for diaganol wins
+            arr[2][0] === activeToken && arr[1][1] === activeToken && arr[0][2] === activeToken) {
+                console.log(`${players.getActivePlayer().name} WINS!!!!`)
+            }
+    }
+    
     board.printBoard()
     return {playTurn}
 }
